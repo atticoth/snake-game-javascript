58463dc9317e92d8d99a9352fba7af074e4284ff
Thiago Attico <thiagoattico@hotmail.com>
2020-07-12 20:34:37 -0300

    Snake Game

---
+++ script.js
@@ -0,0 +1,12 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+
+function criarBG(){
+    // Fundo do jogo
+    context.fillStyle = "lightgreen";
+    //Desenhando o tamanho do jogo
+    context.fillRect(0, 0 , 16 * box, 16 * box);
+
+}
+criarBG();
