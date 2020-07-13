af3ab6388e6e8e72c08553a052a9e4a583d143ae
Thiago <thiagoattico@hotmail.com>
2020-07-12 21:03:11 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+// criando a cobrinha
+let snake = [];
+snake [0] ={
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG(){
     // Fundo do jogo
@@ -10,4 +16,13 @@ function criarBG(){
 
 }
 
+function criarCobrinha(){
+    for (i=0; i < snake.length; i++){
+        // Definindo a cor e o tamanho da cobrinha
+        context.fillStyle = "blue"
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
